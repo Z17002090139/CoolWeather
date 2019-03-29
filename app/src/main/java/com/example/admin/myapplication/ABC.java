af0025bc0807aafc1e3bb6a0bae62940f0e4dd67
admin <aaa>
2019-03-29 09:25:51 +0800

    3/29

--- app/src/main/java/com/example/admin/myapplication/ABC.java
+++ app/src/main/java/com/example/admin/myapplication/ABC.java
@@ -1,15 +1,27 @@
 package com.example.admin.myapplication;
 
 public class ABC {
+    public  static  int count=0;
     public  static void main(String[] args){
-        System.out.println("This is thread one");
 
         Thread thread=new Thread(){
             @Override
             public void run() {
-                System.out.println("This is thread two");
+                System.out.println(count);
+                count=0;
             }
         };
         thread.start();
+        System.out.println("hello world");
+
+            (new Thread(){
+                @Override
+                public void run() {
+                    for (int  i=0;i<5;i++)
+                        count=count+1;
+                    System.out.println("+1");
+                }
+        }).start();
+
     }
 }
